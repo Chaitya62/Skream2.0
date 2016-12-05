5f0e143366c6c9cc522aa97805dc013ca0001455
Chaitya Shah <Chaitya62@gmail.com>
2016-12-05 23:01:02 +0530

    changes nav

--- dist/js/index.js
+++ dist/js/index.js
@@ -53,9 +53,6 @@ var NavbarHandler = {
 			}
 		},
 		eventBind:function(){
-			this.window.on("load",this.fullscreen.bind(this));
-			this.window.on("scroll",this.preventDefaults.bind(this));
-			document.on("touchmove",this.preventDefaults.bind(this));
 			this.toggleButton.on("click",this.toggleNav.bind(this));
 			this.window.on("orientationchange resize",this.calcVH.bind(this));
 		}
